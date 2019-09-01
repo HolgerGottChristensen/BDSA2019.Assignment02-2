80fbeecdce2a437f8d38780ec44991c4388db926
Rasmus Lystrøm <rasmusl@microsoft.com>
2019-09-01 21:30:10 +0200

    Exercise 2

---
+++ BDSA2019.Assignment02.Tests/IteratorsTests.cs
@@ -0,0 +1,8 @@
+using Xunit;
+
+namespace BDSA2019.Assignment02.Tests
+{
+    public class IteratorsTests
+    {
+    }
+}
---
+++ BDSA2019.Assignment02/Iterators.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BDSA2019.Assignment02
+{
+    public static class Helpers
+    {
+        public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
